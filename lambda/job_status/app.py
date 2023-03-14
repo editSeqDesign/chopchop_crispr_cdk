7419c46a8ca5fcd7cd9b397e4dbf3169e8f8b5c3
wangruoyu <wangry@tib.cas.cn>
2023-03-14 02:24:33 +0000

    update result content

--- lambda/job_status/app.py
+++ lambda/job_status/app.py
@@ -1,3 +1,12 @@
+#-*- coding:utf-8 -*-
+'''
+Author: wangruoyu, wangry@tib.cas.cn
+Date: 2023-03-14 01:38:51
+LastEditors: wangruoyu
+LastEditTime: 2023-03-14 01:56:32
+Description: file content
+FilePath: /chopchop_crispr_cdk/lambda/job_status/app.py
+'''
 import os
 import sys
 import boto3
@@ -64,12 +73,11 @@ def lambda_handler(event,context):
             if statusCode == 200:
                 output_file = event["result"][module]['Payload']["output_file"]
                 output_dict[module] = output_file
-                if module == "chopchop":
-                    obj_key = "/".join(output_file.split('/')[3:])
-                    result.append(obj_key)
-                elif module == 'edit':
+                print(output_file)
+                if module == "chopchop" or module == "edit":
                     for i in output_file:
                         obj_key = "/".join(i.split('/')[3:])
+                        print(i,obj_key)
                         result.append(obj_key)
             elif statusCode == 500:
                 msg += event["result"][module]['Payload']["msg"] +"; "
